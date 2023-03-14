c5ca847ae0473031913d123485b4a623986a506d
eMobilis <preciousmwangi34@gmail.com>
2023-03-14 12:29:19 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+    var num1 = 30
+    var num2 = 40
+    var result = num1 * num2
+    print(result)
+
+}
+
+
---
+++ src/main/kotlin/bmicalculator.kt
@@ -0,0 +1,6 @@
+fun main(args: Array<String>) {
+        var mass = 50
+        var height = 2
+        val result = mass / height
+        print(result)
+    }

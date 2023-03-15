d7c63cd372828a357a67b527e4c6eb9ec5e0d263
eMobilis <preciousmwangi34@gmail.com>
2023-03-15 11:41:32 +0300

    Data types in kotlin

---
+++ src/main/kotlin/datatypes.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>) {
+    var age: Int = 20
+    //strings
+    var name:String = "Hans"
+    //Double
+    var temperature:Double = 36.7
+    //Boolean-either true or false
+    var status = false
+    //float
+    var point: Float = 8.5F
+    //Convert double to Float
+    var marks = 46.587F
+}
